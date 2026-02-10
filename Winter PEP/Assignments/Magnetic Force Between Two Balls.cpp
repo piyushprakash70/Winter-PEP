31cab8d679984b87472fc01ee09cfb4b08f96dca
Piyush Prakash <148524223+piyushprakash70@users.noreply.github.com>
2026-02-10 22:46:46 +0530

    Create Magnetic Force Between Two Balls.cpp

---
+++ Winter PEP/Assignments/Magnetic Force Between Two Balls.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canPlace(vector<int>& position, int m, int dist) {
+        int c = 1;                
+        int last = position[0];
+
+        for (int i = 1; i < position.size(); i++) {
+            if (position[i] - last >= dist) {
+                c++;
+                last = position[i];
+                if (c == m)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(), position.end());
+
+        int l = 1;
+        int h = position.back() - position.front();
+        int r = 0;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (canPlace(position, m, mid)) {
+                r = mid;
+                l = mid + 1;    
+            } else {
+                h = mid - 1;   
+            }
+        }
+        return r;
+    }
+};
