682e734cdad42cb53d7721e4c9226b3914f137e8
Piyush Prakash <148524223+piyushprakash70@users.noreply.github.com>
2026-02-26 09:25:02 +0530

    Initial Commit

---
+++ Winter PEP/LeetCode/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* curr1=headA;
+        ListNode* curr2=headB;
+        while(curr1 != curr2)
+        {
+            curr1=curr1?curr1->next:headB;
+            curr2=curr2?curr2->next:headA;
+        }
+        return curr1;
+    }
+};
