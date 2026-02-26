3d4291caf3a9e6d89471b420318ee70dfa25053b
Piyush Prakash <148524223+piyushprakash70@users.noreply.github.com>
2026-02-26 09:41:49 +0530

    Initial Commit

---
+++ Winter PEP/LeetCode/Reverse Nodes in K-Group.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
+        vector<int> v;
+        ListNode* curr = head;
+        while (curr) {
+            v.push_back(curr->val);
+            curr = curr->next;
+        }
+        for (int i = 0; i + k <= v.size(); i += k) {
+            reverse(v.begin() + i, v.begin() + i + k);
+        }
+        curr = head;
+        int in = 0;
+        while (curr) {
+            curr->val = v[in++];
+            curr = curr->next;
+        }
+        return head;
+    }
+};
